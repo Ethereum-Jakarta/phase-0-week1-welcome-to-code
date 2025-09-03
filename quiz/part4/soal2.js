69a279ed91451004f75ba1eb00def80309ded583
Haidir Aditya <haidiraditya16@gmail.com>
2025-09-03 19:31:25 +0800

    mengerjakan semua soal part 4

---
+++ quiz/part4/soal2.js
@@ -0,0 +1,17 @@
+function calculateMultiply(num1, num2) {
+    return num1 * num2;
+}
+
+// ------------------------------------
+console.log('\n')
+// ------------------------------------
+
+let num1 = 3;
+let num2 = 2;
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+// ------------------------------------
+console.log('\n')
+// -----------------------------------
