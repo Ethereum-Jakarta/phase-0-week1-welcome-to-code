778b6561c46e91772eb13b1f425c59cabf79256b
ihsan <75242608+ihsanoo@users.noreply.github.com>
2026-01-15 08:10:52 +0800

    week 1

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,26 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+console.log("LOOPING PERTAMA")
+console.log('')
+let angka = 1;
+
+while(angka <= 5) {
+    console.log(angka);
+    angka++;
+}
+
+console.log(angka)
+
+console.log("LOOPING KEDUA");
+console.log('');
+
+angka = 5;
+
+while (angka >= 1) {
+    console.log(angka);
+    angka--;
+}
