dc88de68c1c89357ce45af0834474f329eea2aee
Indayani <davanalendra2@gmail.com>
2026-01-27 17:22:03 +0700

    finishing jawab3

--- quiz/part3/jawab3.js
+++ quiz/part3/jawab3.js
@@ -4,4 +4,14 @@
 // Di dalam perulangan, periksa setiap angka counter:
 // Apabila angka counter adalah angka genap, tuliskan GENAP
 // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
- 
+
+let teks1 = "GENAP";
+let teks2 = "GANJIL";
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) 
+    console.log (teks1); 
+    if (i % 2 !== 0)
+    console.log (teks2); {
+        console.log (i);
+    }
+}
