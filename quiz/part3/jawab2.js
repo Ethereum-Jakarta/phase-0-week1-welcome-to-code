ad61303c605037e56239b6c537b4b87ab1c933b0
Indayani <davanalendra2@gmail.com>
2026-01-27 16:57:42 +0700

    finishing question jawab2

--- quiz/part3/jawab2.js
+++ quiz/part3/jawab2.js
@@ -3,3 +3,15 @@
 // Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+let judul1 = "LOOPING PERTAMA";
+console.log (judul1);
+for (let i = 0; i <= 5; i++) {
+    console.log (i);
+}
+
+let judul2 = "LOOPING KEDUA";
+console.log (judul2);
+for (let j = 5; j >= 1; j--) {
+    console.log (j);
+}
