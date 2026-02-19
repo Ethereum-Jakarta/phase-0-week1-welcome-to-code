d62f19b43526df0586c003b86d5abc0b31f35567
ardii <192996411+ardiw1123@users.noreply.github.com>
2026-02-19 22:53:25 +0700

    submit quiz part 3

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,10 @@
+let angka = 0;
+while (angka <= 10) {
+  console.log("hitung maju:", angka++);
+}
+console.log("LOOPING PERTAMA");
+let angka2 = 10;
+while (angka2 >= 0) {
+  console.log("hitung mundur:", angka2--);
+}
+console.log("LOOPING KEDUA");
