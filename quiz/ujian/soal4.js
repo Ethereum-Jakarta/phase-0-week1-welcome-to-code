ec3381da49f62c092fac620f8fc8ad6300541a12
ardii <192996411+ardiw1123@users.noreply.github.com>
2026-02-22 10:27:50 +0700

    submit ujian

---
+++ quiz/ujian/soal4.js
@@ -0,0 +1,20 @@
+function xo(str) {
+  // you can only write your code here!
+  let countX = 0,
+    countO = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "o") {
+      countO++;
+    } else if (str[i] === "x") {
+      countX++;
+    }
+  }
+  return countO === countX;
+}
+
+// TEST CASES
+console.log(xo("xoxoxo")); // true
+console.log(xo("oxooxo")); // false
+console.log(xo("oxo")); // false
+console.log(xo("xxxooo")); // true
+console.log(xo("xoxooxxo")); // true
