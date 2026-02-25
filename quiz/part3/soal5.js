1e8a4f0243e5ada91785198716deae875c0b7d68
Im-sulistio <imronsulistio43@gmail.com>
2026-02-25 12:54:04 +0700

    submit tugas part3

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,18 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+let bintang = "";
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    bintang += "*";
+  }
+  bintang += "\n";
+}
+
+console.log(bintang);
