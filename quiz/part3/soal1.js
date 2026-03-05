d332b45170828525d278856d1b1861268e135ee4
kfnx <kafinsalim@gmail.com>
2026-03-05 20:45:28 +0700

    Complete all arcane quests and exam

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,15 @@
+// LOOPING PERTAMA
+let i = 1;
+while (i <= 5) {
+    console.log(i + ' - Looping maju');
+    i++;
+}
+
+console.log('');
+
+// LOOPING KEDUA
+let j = 5;
+while (j >= 1) {
+    console.log(j + ' - Looping mundur');
+    j--;
+}
